7d2b6ebe5cba2292802f487d9143fedc63779448
DanORLarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-09-19 13:45:54 +0200

    First greenbar (ENUM gives the right string)

--- Test/src/TestTest.java
+++ Test/src/TestTest.java
@@ -3,9 +3,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestTest {
+// 1 -Check that each status has an appropriate textual description, i.e. “Docked at home”, “Docked away”, and “On voyage...”.
+
 
     @Test
-    void test(){
-        
+    void checkENUMStatus(){
+        assertTrue(ENUMstatus.HOME.toString() == "Docked at home");
     }
 }
---
+++ src/ENUMstatus.java
@@ -0,0 +1,11 @@
+public enum ENUMstatus {
+    HOME{ public String toString(){
+        return "Docked at home";
+    }},AWAY{ public String toString(){
+        return "Docked away";
+    }}, VOYAGE{ public String toString(){
+        return "On voyage...";
+    }};
+
+
+}
